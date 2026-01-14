489c39dba787415337151560f8b28a0beac59cbe
David Sutton <davidsutton@ausocean.org>
2026-01-15 10:15:01 +1030

    Speaker Netsender: Initialise SD Card
    This change adds an initialisation function for the SD card. This change
    also adds a kconfig.projbuild file which adds configuration values which
    can be used to easily change configurations between devices.

--- esp/esp/main/main.c
+++ esp/esp/main/main.c
@@ -9,6 +9,14 @@
 #include "esp_event.h"
 #include <sdkconfig.h>
 #include "esp_log.h"
+#include "sdmmc_cmd.h"
+#include "driver/sdspi_host.h"
+#include "esp_vfs_fat.h"
+
+#define SPEAKER_VERSION "0.0.1"
+
+// Mount point for the SD card filesystem.
+#define MOUNT_POINT "/sdcard"
 
 static const char* TAG = "speaker";
 
@@ -56,10 +64,8 @@ static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
     ESP_LOGI(TAG, "~~~~~~~~~~~");
 }
 
-void app_main(void)
-{
-    ESP_LOGI(TAG, "Initialising speaker netsender");
-
+/** Initialisation of the ethernet MAC, PHY, and IP/TCP */
+static void init_ethernet() {
     eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();                      // apply default common MAC configuration
     eth_esp32_emac_config_t esp32_emac_config = ETH_ESP32_EMAC_DEFAULT_CONFIG(); // apply default vendor-specific MAC configuration
     esp32_emac_config.smi_gpio.mdc_num = CONFIG_ETHERNET_MDC_GPIO;            // alter the GPIO used for MDC signal
@@ -85,4 +91,64 @@ void app_main(void)
     esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handle)); // attach Ethernet driver to TCP/IP stack
     esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL); // register user defined IP event handlers
     esp_eth_start(eth_handle); // start Ethernet driver state machine
+}
+
+static void init_sd() {
+    // Use the default host.
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+
+    // Configure the SPI bus to use the config values.
+    spi_bus_config_t bus_cfg = {
+        .mosi_io_num = CONFIG_SD_MOSI,
+        .miso_io_num = CONFIG_SD_MISO,
+        .sclk_io_num = CONFIG_SD_CLK,
+        .quadwp_io_num = CONFIG_SD_QUADWP,
+        .quadhd_io_num = CONFIG_SD_QUADHD,
+        .max_transfer_sz = CONFIG_SD_MAX_TRANSFER_SZ,
+    };
+
+    // Initialise the SPI Bus.
+    ESP_ERROR_CHECK(spi_bus_initialize(host.slot, &bus_cfg, SDSPI_DEFAULT_DMA));
+
+    // Configure the SD Slot.
+    sdspi_dev_handle_t sd_handle;
+    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_config.gpio_cs = CONFIG_SD_CS;
+    slot_config.gpio_cd = CONFIG_SD_DET;
+    slot_config.host_id = host.slot;
+
+    // Initialise the 
+    ESP_ERROR_CHECK(sdspi_host_init_device(&slot_config, &sd_handle));
+
+    ESP_LOGI(TAG, "Mounting filesystem");
+    
+
+    sdmmc_card_t *card;
+    const char mount_point[] = MOUNT_POINT;
+
+    // Options for mounting the filesystem.
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = false,
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+
+    ESP_ERROR_CHECK(esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &card));
+
+    ESP_LOGI(TAG, "Filesystem mounted");
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+}
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Speaker Netsender Version: %s", SPEAKER_VERSION);
+
+    ESP_LOGI(TAG, "Initialising ethernet");
+    init_ethernet();
+    ESP_LOGI(TAG, "Ethernet initialised");
+
+    ESP_LOGI(TAG, "Initialising SD card");
+    init_sd();
 }
