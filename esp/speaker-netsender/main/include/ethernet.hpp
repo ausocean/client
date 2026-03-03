45f607e702323312e9473b40c53ca4ec3b9c4403
David Sutton <davidsutton@ausocean.org>
2026-03-03 16:44:28 +1030

    ESP/Speaker: Factor out ethernet functions
    This change moves all ethernet related functions to an external file to
    clean up the main file.

---
+++ esp/speaker-netsender/main/include/ethernet.hpp
@@ -0,0 +1,45 @@
+/*
+  Name:
+    ethernet.hpp - Ethernet functions for the ESP Speaker.
+
+  Authors:
+    David Sutton <davidsutton@ausocean.org>
+
+  License:
+    Copyright (C) 2026 The Australian Ocean Lab (AusOcean).
+
+    This file is part of NetSender. NetSender is free software: you can
+    redistribute it and/or modify it under the terms of the GNU
+    General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    NetSender is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NetSender in gpl.txt.  If not, see
+    <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "esp_event_base.h"
+
+/**
+ * @brief Event handler for ethernet events.
+ */
+void eth_event_handler(void *, esp_event_base_t, int32_t event_id, void *event_data);
+
+/**
+ * @brief Event handler for IP events.
+ */
+void got_ip_event_handler(void *, esp_event_base_t,
+                          int32_t, void *event_data);
+
+/**
+ * @brief Initialise ethernet MAC, PHY and TCP/IP.
+ */
+void init_ethernet();
--- esp/speaker-netsender/main/main.cpp
+++ esp/speaker-netsender/main/main.cpp
@@ -34,6 +34,7 @@ extern "C" {
 #include "netsender.hpp"
 #include "include/netsender_vars.hpp"
 #include "include/audio.hpp"
+#include "include/ethernet.hpp"
 #include "soc/clk_tree_defs.h"
 #include "driver/i2s_types.h"
 #include "esp_err.h"
@@ -81,94 +82,6 @@ static TaskHandle_t player_handle;
 // Atomic flag for stopping audio playback.
 // TODO: Use a better threadsafe option.
 volatile bool reload_requested = false;
-
-// Event handler for Ethernet events.
-static void eth_event_handler(void *, esp_event_base_t, int32_t event_id, void *event_data)
-{
-    uint8_t mac_addr[6] = {0};
-
-    switch (event_id) {
-    case ETHERNET_EVENT_CONNECTED: {
-        auto eth_handle = *(esp_eth_handle_t *)event_data;
-        esp_eth_ioctl(eth_handle, ETH_CMD_G_MAC_ADDR, mac_addr);
-        ESP_LOGI(TAG, "Ethernet Link Up");
-        ESP_LOGI(TAG, "Ethernet HW Addr %02x:%02x:%02x:%02x:%02x:%02x",
-                 mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-    }
-    break;
-    case ETHERNET_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "Ethernet Link Down");
-        break;
-    case ETHERNET_EVENT_START:
-        ESP_LOGI(TAG, "Ethernet Started");
-        break;
-    case ETHERNET_EVENT_STOP:
-        ESP_LOGI(TAG, "Ethernet Stopped");
-        break;
-    default:
-        break;
-    }
-
-}
-
-// Event handler for IP_EVENT_ETH_GOT_IP.
-static void got_ip_event_handler(void *, esp_event_base_t,
-                                 int32_t, void *event_data)
-{
-    auto *event = (ip_event_got_ip_t *) event_data;
-    const auto *ip_info = &event->ip_info;
-
-    ESP_LOGI(TAG, "Ethernet Got IP Address");
-    ESP_LOGI(TAG, "~~~~~~~~~~~");
-    ESP_LOGI(TAG, "ETHIP:" IPSTR, IP2STR(&ip_info->ip));
-    ESP_LOGI(TAG, "ETHMASK:" IPSTR, IP2STR(&ip_info->netmask));
-    ESP_LOGI(TAG, "ETHGW:" IPSTR, IP2STR(&ip_info->gw));
-    ESP_LOGI(TAG, "~~~~~~~~~~~");
-}
-
-// Initialisation of the ethernet MAC, PHY, and IP/TCP.
-static void init_ethernet()
-{
-    // Create MAC instance.
-    eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
-    eth_esp32_emac_config_t esp32_emac_config = ETH_ESP32_EMAC_DEFAULT_CONFIG();
-    esp32_emac_config.smi_gpio.mdc_num = CONFIG_ETHERNET_MDC_GPIO;
-    esp32_emac_config.smi_gpio.mdio_num = CONFIG_ETHERNET_MDIO_GPIO;
-    auto *mac = esp_eth_mac_new_esp32(&esp32_emac_config, &mac_config);
-
-    // Create PHY instance (LAN8720).
-    eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
-    phy_config.phy_addr = CONFIG_ETHERNET_PHY_ADDR;
-    phy_config.reset_gpio_num = CONFIG_ETHERNET_PHY_RST_GPIO;
-    auto *phy = esp_eth_phy_new_lan87xx(&phy_config);
-
-    // Install driver.
-    esp_eth_config_t config = ETH_DEFAULT_CONFIG(mac, phy);
-    esp_eth_handle_t eth_handle = NULL;
-    ESP_ERROR_CHECK(esp_eth_driver_install(&config, &eth_handle));
-
-    // Create a default event loop that runs in the background.
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    // Register Ethernet event handler.
-    ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
-
-    // Initialize TCP/IP network interface.
-    ESP_ERROR_CHECK(esp_netif_init());
-
-    // Create network interface for Ethernet driver.
-    esp_netif_config_t cfg = ESP_NETIF_DEFAULT_ETH();
-    auto *eth_netif = esp_netif_new(&cfg);
-
-    // Attach Ethernet driver to TCP/IP stack.
-    ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handle)));
-
-    // Register user defined IP event handlers.
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
-
-    // Start Ethernet driver state machine.
-    ESP_ERROR_CHECK(esp_eth_start(eth_handle));
-}
 
 static auto init_sd()
 {
