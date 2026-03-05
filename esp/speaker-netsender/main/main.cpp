d61854e334ae741b43f987f7a1677578aee02c33
David Sutton <david@ausocean.org>
2026-03-05 16:19:56 +1030

    ESP/Speaker: Read vars from SD card

--- esp/speaker-netsender/main/main.cpp
+++ esp/speaker-netsender/main/main.cpp
@@ -65,7 +65,7 @@ extern "C" {
 #include "esp_vfs_fat.h"
 
 // Current version of the speaker.
-static constexpr const auto SPEAKER_VERSION = "0.3.1";
+static constexpr const auto SPEAKER_VERSION = "0.3.2";
 
 // File to save variables to.
 static const constexpr auto VARS_FILE = "variables.txt";
@@ -140,6 +140,18 @@ void app_main(void)
     ESP_LOGI(TAG, "Initialising I2S Amp");
     auto amp = init_amp();
     ESP_LOGI(TAG, "Amp Initialised");
+
+    // Get any stored variables from the SD card when it exists.
+    constexpr const auto MAX_VAR_PATH_LEN = 64;
+    char var_file[MAX_VAR_PATH_LEN];
+    snprintf(var_file, MAX_VAR_PATH_LEN, "%s/%s", MOUNT_POINT, VARS_FILE);
+    auto got_vars = netsender::read_vars_from_file(var_file);
+    if (!got_vars.has_value()) {
+        ESP_LOGW(TAG, "got no variables from SD");
+    } else {
+        ESP_LOGI(TAG, "got variables from SD");
+        vars = got_vars.value();
+    }
 
     // Start the Audio Task.
     xTaskCreatePinnedToCore(audio_task, "audio_task", 4096, &amp, 5, &player_handle, 1);
