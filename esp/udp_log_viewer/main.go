fa86f697c4f4ee81944128fc660dcb07a5f4c035
David Sutton <david@ausocean.org>
2026-03-16 09:15:19 +1030

    Log Viewer: Add file header

--- esp/udp_log_viewer/main.go
+++ esp/udp_log_viewer/main.go
@@ -1,3 +1,29 @@
+/*
+  Name:
+    UDP Log Viewer - CLI tool for viewing, and saving logs sent via UDP.
+
+  Authors:
+    David Sutton <davidsutton@ausocean.org>
+
+  License:
+    Copyright (C) 2026 The Australian Ocean Lab (AusOcean).
+
+    This file is part of NetSender. NetSender is free software: you can
+    redistribute it and/or modify it under the terms of the GNU
+    General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    NetSender is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NetSender in gpl.txt.  If not, see
+    <http://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
