2584471346b94d56623cdfa684864f28ae149268
David (AusOcean) <160693812+ao-david@users.noreply.github.com>
2026-03-25 08:59:25 +1030

    ESP/Speaker: Add self check capability (#125)
    This change adds a self check function which is used to check that all
    devices are connected and operating as expected. This is intended to
    prove firmware validity, but may also be useful to diagnose hardware
    issues.

---
+++ esp/speaker-netsender/main/include/self_check.hpp
@@ -0,0 +1,36 @@
+/*
+  Name:
+    self_check.hpp - Self check functions to validate hardware and
+    firmware compatibility.
+
+  Authors:
+    David Sutton <davidsutton@ausocean.org>
+
+  License:
+    Copyright (C) 2026 The Australian Ocean Lab (AusOcean).
+
+    This file is part of NetSender. NetSender is free software: you can
+    redistribute it and/or modify it under the terms of the GNU
+    General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    NetSender is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NetSender in gpl.txt.  If not, see
+    <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "tas5805.hpp"
+
+/**
+ * @brief checks all peripherals and network access to validate
+ * hardware and firmware compatibility.
+ */
+bool self_check_ok(TAS5805 *amp);
