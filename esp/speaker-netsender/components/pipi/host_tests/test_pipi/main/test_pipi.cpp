35fb86c9e0b67a2f5d6bd4086da22d56476e9f36
David Sutton <david@ausocean.org>
2026-04-27 13:18:27 +0930

    ESP/Speaker: Implement File Logging
    This change adds the basics of logging timestamped logs to a file.

--- esp/speaker-netsender/components/pipi/host_tests/test_pipi/main/test_pipi.cpp
+++ esp/speaker-netsender/components/pipi/host_tests/test_pipi/main/test_pipi.cpp
@@ -1,13 +1,36 @@
+#include <chrono>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <ios>
+#include <limits>
 #include <sstream>
+#include <thread>
+#include <unistd.h>
 
-#include "esp_log.h"
+#include "esp_err.h"
 
 #include "gtest/gtest.h"
 #include "pipi.hpp"
 
 namespace Pipi {
+// Forward declarations
+void assert_log_level(std::ifstream& f, int level);
 
 class TestEntry : public testing::Test {
+  protected:
+    static constexpr auto TEST_DIR = "test";
+    void SetUp() override
+    {
+        std::filesystem::create_directory(TEST_DIR);
+        chdir(TEST_DIR);
+    }
+    void TearDown() override
+    {
+        chdir("../");
+        std::filesystem::remove_all(TEST_DIR);
+    }
+
   public:
     static constexpr auto TEST_TIME = 1064;
 };
@@ -45,4 +68,107 @@ TEST_F(TestEntry, Write)
     EXPECT_STREQ(want, got);
 }
 
+class TestFileLogger : public testing::Test {
+  protected:
+    static constexpr auto TEST_DIR = "test";
+    void SetUp() override
+    {
+        std::filesystem::create_directory(TEST_DIR);
+        chdir(TEST_DIR);
+    }
+    void TearDown() override
+    {
+        chdir("../");
+        // std::filesystem::remove_all(TEST_DIR);
+    }
+};
+
+TEST_F(TestFileLogger, Create_Good_Path)
+{
+    constexpr auto LOG_DIR = "tmp";
+    auto p = FileLogger(LOG_DIR);
+    ASSERT_TRUE(p.ready);          // FileLogger is ready.
+    ASSERT_STREQ(p.path, LOG_DIR); // Log path is as passed.
+}
+
+TEST_F(TestFileLogger, Create_With_No_Path)
+{
+    auto p = FileLogger();
+    ASSERT_TRUE(p.ready);                     // FileLogger is ready.
+    ASSERT_STREQ(p.path, Pipi::DEFAULT_PATH); // Log path is as passed.
+}
+
+TEST_F(TestFileLogger, Create_Empty_Path)
+{
+    constexpr auto LOG_DIR = "";
+    auto p = FileLogger(LOG_DIR);
+    ASSERT_TRUE(p.ready);                     // FileLogger is ready.
+    ASSERT_STREQ(p.path, Pipi::DEFAULT_PATH); // Log path is as passed.
+}
+
+TEST_F(TestFileLogger, Make_Path)
+{
+    constexpr auto LOG_DIR = "long/path/to/logs";
+    auto p = FileLogger(LOG_DIR);
+
+    struct stat buffer;
+    stat(LOG_DIR, &buffer);
+    ASSERT_TRUE(S_ISDIR(buffer.st_mode)); // Log Dir was created.
+}
+
+TEST_F(TestFileLogger, Write_Logs_Direct)
+{
+    constexpr auto LOG_DIR = "write_logs_direct";
+    auto p = FileLogger(LOG_DIR);
+
+    constexpr auto TEST_INT = 1234;
+    auto err = p.log(Level::INFO, "This is an INFO log with an int (%d)", TEST_INT);
+    ASSERT_EQ(err, ESP_OK);
+    err = p.log(Level::INFO, "This is an INFO log with no formatting");
+    ASSERT_EQ(err, ESP_OK);
+    err = p.log(Level::WARN, "This is a WARN log with an int (%d)", TEST_INT);
+    ASSERT_EQ(err, ESP_OK);
+    err = p.log(Level::WARN, "This is a WARN log with no formatting");
+    ASSERT_EQ(err, ESP_OK);
+    err = p.log(Level::ERROR, "This is an ERROR log with an int (%d)", TEST_INT);
+    ASSERT_EQ(err, ESP_OK);
+    err = p.log(Level::ERROR, "This is an ERROR log with no formatting");
+    ASSERT_EQ(err, ESP_OK);
+    err = p.log(Level::FATAL, "This is a FATAL log with an int (%d)", TEST_INT);
+    ASSERT_EQ(err, ESP_OK);
+    err = p.log(Level::FATAL, "This is a FATAL log with no formatting");
+    ASSERT_EQ(err, ESP_OK);
+
+    // This is required because logfiles are timestamped, and will overwrite itself if called too quickly.
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    // // Create a new logfile so we can read from the old one.
+    p.new_file();
+    ASSERT_TRUE(p.prev_file.is_open());
+
+    // Assert the log levels.
+    for (auto i = 0; i < 2; i++) {
+        assert_log_level(p.prev_file, Level::INFO);
+    }
+    for (auto i = 0; i < 2; i++) {
+        assert_log_level(p.prev_file, Level::WARN);
+    }
+    for (auto i = 0; i < 2; i++) {
+        assert_log_level(p.prev_file, Level::ERROR);
+    }
+    for (auto i = 0; i < 2; i++) {
+        assert_log_level(p.prev_file, Level::FATAL);
+    }
+}
+
+void assert_log_level(std::ifstream& f, int level) {
+    constexpr auto SEARCH_LEN = 1024;
+    f.ignore(SEARCH_LEN, ','); // Skip over timestamp
+    char got[SEARCH_LEN];
+    f.getline(got, SEARCH_LEN, ',');
+
+    char level_str[10];
+    snprintf(level_str, 10, "\"level\":%d", level);
+    ASSERT_STREQ(got, level_str);
+}
 } // namespace Pipi
--- esp/speaker-netsender/components/pipi/pipi.cpp
+++ esp/speaker-netsender/components/pipi/pipi.cpp
@@ -29,10 +29,32 @@
 
 #include "include/pipi.hpp"
 
-#include <cinttypes>
+#include <cerrno>
+#include <chrono>
+#include <cstdarg>
+#include <cstdio>
 #include <cstring>
+#include <fstream>
+#include <ostream>
+#include <sys/stat.h>
 
 #include "esp_err.h"
+#include "esp_log.h"
+
+// Logging Tag.
+static constexpr auto TAG = "Pipi";
+
+Pipi::Entry::Entry(const int64_t ts, const Level level, const char *fmt, va_list args) : timestamp(ts), level(level)
+{
+    char msg[Entry::MAX_LOG_LENGTH];
+    auto written = vsnprintf(msg, Entry::MAX_LOG_LENGTH, fmt, args);
+    if (written < 0) {
+        ESP_LOGE(TAG, "unable to write message to Entry");
+        return;
+    }
+
+    Entry(ts, level, msg);
+}
 
 Pipi::Entry::Entry(const int64_t ts, const Level level, const char *msg) : timestamp(ts), level(level), data(msg) {}
 
@@ -57,6 +79,145 @@ esp_err_t Pipi::Entry::write(std::ostream &stream)
     }
 
     stream.write(marshalled, written);
+    if (stream.fail()) {
+        return ESP_FAIL;
+    }
+    return ESP_OK;
+}
+
+esp_err_t Pipi::FileLogger::make_path(const char *path)
+{
+    auto making = true;
+    char parent[Pipi::FileLogger::MAX_PATH_LENGTH];
+    errno = 0;
+    auto loc = path;
+    while (making) {
+        // find next directory string.
+        loc = strchr(&loc[1], '/');
+        if (loc == NULL) {
+            loc = &path[strlen(path)];
+            making = false;
+        }
+
+        auto len = loc - path;
+        snprintf(parent, len + 1, "%s", path);
+
+        auto status = mkdir(parent, 0777);
+        if (status == -1 && errno != EEXIST) {
+            ESP_LOGE(TAG, "unable to create dir (%s): %d (%s)", parent, errno, strerror(errno));
+            return ESP_FAIL;
+        }
+    }
 
     return ESP_OK;
+}
+
+Pipi::FileLogger::FileLogger(const char *path) : ready(false)
+{
+    // Validate the path is a directory.
+    struct stat buffer;
+    errno = 0;
+    auto status = stat(path, &buffer);
+
+    if (status == -1 && errno == ENOENT && strlen(path) > 0) {
+        // Directory doesn't exist so create it.
+        auto err = make_path(path);
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG, "unable to create path");
+            return;
+        }
+        strncpy(this->path, path, this->MAX_PATH_LENGTH);
+    } else if (S_ISDIR(buffer.st_mode)) {
+        // The given path is a directory so use it.
+        strncpy(this->path, path, this->MAX_PATH_LENGTH);
+    } else {
+        // The given path points to a file, so use the default.
+        ESP_LOGW(TAG, "path for FileLogger must be a directory, defaulting to %s", DEFAULT_PATH);
+        auto err = make_path(DEFAULT_PATH);
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG, "unable to create path");
+            return;
+        }
+        strncpy(this->path, DEFAULT_PATH, this->MAX_PATH_LENGTH);
+    }
+
+    // Start a new logging file.
+    auto err = this->new_file();
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "unable to start new logfile: %s", esp_err_to_name(err));
+        return;
+    }
+
+    ready = true;
+}
+
+Pipi::FileLogger::FileLogger() : Pipi::FileLogger(DEFAULT_PATH) {}
+
+esp_err_t Pipi::FileLogger::new_file()
+{
+    if (this->prev_file.is_open()) {
+        this->prev_file.close();
+    }
+    if (this->curr_file.is_open()) {
+        this->curr_file.close();
+        this->prev_file.open(curr_file_path);
+        if (!this->prev_file.is_open()) {
+            ESP_LOGE(TAG, "unable to open previous file as ifstream");
+            return ESP_FAIL;
+        }
+    }
+
+    snprintf(this->curr_file_path, MAX_PATH_LENGTH, "%s/%" PRId64 ".log", this->path, time(nullptr));
+    this->curr_file.open(this->curr_file_path, std::ios::out);
+
+    if (!this->curr_file.is_open()) {
+        ESP_LOGE(TAG, "unable to open log file: %s", this->curr_file_path);
+        return ESP_FAIL;
+    }
+
+    return ESP_OK;
+}
+
+esp_err_t Pipi::FileLogger::info(const char *msg) { return this->log(Level::INFO, msg); }
+esp_err_t Pipi::FileLogger::warn(const char *msg) { return this->log(Level::WARN, msg); }
+esp_err_t Pipi::FileLogger::error(const char *msg) { return this->log(Level::ERROR, msg); }
+esp_err_t Pipi::FileLogger::fatal(const char *msg) { return this->log(Level::FATAL, msg); }
+
+esp_err_t Pipi::FileLogger::info(const char *msg, va_list args) { return this->log(Level::INFO, msg, args); }
+esp_err_t Pipi::FileLogger::warn(const char *msg, va_list args) { return this->log(Level::WARN, msg, args); }
+esp_err_t Pipi::FileLogger::error(const char *msg, va_list args) { return this->log(Level::ERROR, msg, args); }
+esp_err_t Pipi::FileLogger::fatal(const char *msg, va_list args) { return this->log(Level::FATAL, msg, args); }
+
+esp_err_t Pipi::FileLogger::log(const Pipi::Level level, const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    if (!this->ready) {
+        va_end(args);
+        return ESP_FAIL;
+    }
+
+    char msg[Entry::MAX_LOG_LENGTH];
+    auto written = vsnprintf(msg, Entry::MAX_LOG_LENGTH, fmt, args);
+    va_end(args);
+    if (written < 0) {
+        ESP_LOGE(TAG, "unable to format log message");
+        return ESP_FAIL;
+    }
+
+    std::chrono::system_clock::now();
+    auto e = Entry(time(nullptr), level, msg);
+
+    return e.write(curr_file);
+}
+
+void Pipi::FileLogger::close()
+{
+    if (this->curr_file.is_open()) {
+        this->curr_file.close();
+    }
+    if (this->prev_file.is_open()) {
+        this->prev_file.close();
+    }
+    this->ready = false;
 }
