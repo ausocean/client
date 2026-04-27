011438fedab7cdf099902e7cebe4f6e1d5451a43
David Sutton <david@ausocean.org>
2026-04-27 13:18:27 +0930

    ESP/Speaker: Implement File Logging
    This change adds the basics of logging timestamped logs to a file.

--- esp/speaker-netsender/components/pipi/include/pipi.hpp
+++ esp/speaker-netsender/components/pipi/include/pipi.hpp
@@ -24,6 +24,8 @@
     <http://www.gnu.org/licenses/>.
 */
 
+#include <cstdarg>
+#include <fstream>
 #include <gtest/gtest_prod.h>
 #include <ostream>
 
@@ -50,7 +52,19 @@ class TestEntry;
  * severity level and the text data.
  */
 class Entry {
+    friend class FileLogger;
+
   public:
+    /**
+     * @brief constructor with timestamp, log level and message.
+     *
+     * @param[in] ts unix timestamp (seconds)
+     * @param[in] level enum log level
+     * @param[in] msg format string
+     * @param[in] args format arguments
+     */
+    Entry(const int64_t ts, const Level level, const char *fmt, va_list args);
+
     /**
      * @brief constructor with timestamp, log level and message.
      *
@@ -102,6 +116,124 @@ class Entry {
      * @brief Text Data.
      */
     const char *data;
+};
+
+// Path used for logs if none, or bad path provided.
+constexpr auto DEFAULT_PATH = "log";
+
+class FileLogger {
+  public:
+    /**
+     * @brief Create a new Pipi FileLogger with the given logging location.
+     *
+     * @param[in] path directory to store log files.
+     */
+    FileLogger(const char *path);
+
+    /**
+     * @brief Create a new Pipi FileLogger with the default logging location.
+     */
+    FileLogger();
+
+    /**
+     * @brief Log an info level log.
+     *
+     * @param[in] msg format string for log message.
+     * @param[in] args arguments for format string.
+     */
+    esp_err_t info(const char *msg, va_list args);
+    esp_err_t info(const char *msg);
+
+    /**
+     * @brief Log a warn level log.
+     *
+     * @param[in] msg format string for log message.
+     * @param[in] args arguments for format string.
+     */
+    esp_err_t warn(const char *msg, va_list args);
+    esp_err_t warn(const char *msg);
+
+    /**
+     * @brief Log an error level log.
+     *
+     * @param[in] msg format string for log message.
+     * @param[in] args arguments for format string.
+     */
+    esp_err_t error(const char *msg, va_list args);
+    esp_err_t error(const char *msg);
+
+    /**
+     * @brief Log a fatal level log.
+     *
+     * @param[in] msg format string for log message.
+     * @param[in] args arguments for format string.
+     */
+    esp_err_t fatal(const char *msg, va_list args);
+    esp_err_t fatal(const char *msg);
+
+    /**
+     * @brief maximum path length.
+     */
+    static constexpr auto MAX_PATH_LENGTH = 256;
+
+  private:
+    friend class TestFileLogger;
+
+    FRIEND_TEST(TestFileLogger, Create_Good_Path);
+    FRIEND_TEST(TestFileLogger, Create_With_No_Path);
+    FRIEND_TEST(TestFileLogger, Create_Empty_Path);
+    FRIEND_TEST(TestFileLogger, Make_Path);
+    FRIEND_TEST(TestFileLogger, Write_Logs_Direct);
+
+    /**
+     * @brief Log a formatted message for a given log level.
+     *
+     * @param[in] level enum log level.
+     * @param[in] msg format string for log message.
+     * @param[in] args arguments for format string.
+     */
+    esp_err_t log(const Level level, const char *msg, ...);
+
+    /**
+     * @brief Create a new log file with a timestamped name.
+     *
+     * This updates the curr and prev files.
+     */
+    esp_err_t new_file();
+
+    /**
+     * @brief current logging file.
+     */
+    std::ofstream curr_file;
+
+    /**
+     * @brief path to current log file.
+     */
+    char curr_file_path[MAX_PATH_LENGTH];
+
+    /**
+     * @brief previous logging file.
+     */
+    std::ifstream prev_file;
+
+    /**
+     * @brief path for log files.
+     */
+    char path[MAX_PATH_LENGTH];
+
+    /**
+     * @brief make all parent directories of the given path.
+     *
+     * @param[in] path path to create.
+     */
+    esp_err_t make_path(const char *path);
+
+    /**
+     * @brief tracks if the creation has completed successfully.
+     */
+    bool ready;
+
+    void close();
 };
 
 } // namespace Pipi
